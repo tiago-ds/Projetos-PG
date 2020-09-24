be65ee2aef5c990de45b2ef694c24a0106daae2f
Tiago <tscs2@cin.ufpe.br>
2020-09-23 22:40:17 -0300

    Agora o ponto roda.

--- PROJETO 1/q3/sketch.js
+++ PROJETO 1/q3/sketch.js
@@ -1,44 +1,58 @@
 let pontinho;
-const WIDTH = 800;
-const HEIGHT = 800;
-const OX = WIDTH/2;
-const OY = HEIGHT/2;
+const WIDTH = 500;
+const HEIGHT = 500;
+const oX = WIDTH/2;
+const oY = HEIGHT/2;
 
 function setup() {
+  frameRate(300);
+  
+  // Cria canvas
   createCanvas(WIDTH, HEIGHT);
   background(100);
-  pontinho = new Ponto(-20,0.5);
+  
+  // OX e OY
   stroke(10, 10, 250);
   line(0, HEIGHT/2, WIDTH, HEIGHT/2);
   stroke(10, 250, 10);
   line(WIDTH/2, 0, WIDTH/2, HEIGHT);
-  frameRate(15);
+  
+  /* Move os eixos para o centro
+  /  de forma a deixar as coordenadas
+  /  mais intuitivas
+  */
+  //translate(WIDTH/2, HEIGHT/2);
+  
+  // Cria o ponto de início
+  pontinho = new Ponto(WIDTH/2 - 20, HEIGHT/2);
 }
 
 function draw() {
+  //translate(WIDTH/2, HEIGHT/2);
   stroke(250, 10, 10);
   strokeWeight(5);
   pontinho.display();
   pontinho.update();
-  
 }
 
 class Ponto {
   constructor(x, y) {
     this.posicao = new createVector(x, y);
+    this.angulo = 0.1;
+    this.rotacao = 10;
+    this.giro = 0.5;
+    this.grow = this.giro*5;
   }
   update(){
+    this.angulo+=this.giro;
+    this.rotacao += this.grow;
+    this.posicao.x = oX + cos(this.angulo)*this.rotacao;
+    this.posicao.y = oY -sin(this.angulo)*this.rotacao;
+    if(frameCount%50 == 0){
+      this.angulo*=2;
+    }
   }
   display(){
-    let step = frameCount % 20;
-    let angle = map(step, 0, 20, 0, TWO_PI);
-    let cos_a = cos(angle);
-    let sin_a = sin(angle);
-    translate(WIDTH/2, HEIGHT/2);
-    applyMatrix(cos_a, -sin_a, sin_a, cos_a, 0, 0);
-    if(step == 0){
-      this.posicao.x *=2; 
-    }
     point(this.posicao.x, this.posicao.y);
   }
 }
