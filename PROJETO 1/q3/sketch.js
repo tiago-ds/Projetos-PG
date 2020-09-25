b4372942c4afe77e983658630ca61f8afd87026f
Tiago <tscs2@cin.ufpe.br>
2020-09-24 22:08:13 -0300

    Melhora lógica

--- PROJETO 1/q3/sketch.js
+++ PROJETO 1/q3/sketch.js
@@ -27,30 +27,50 @@ function draw() {
   stroke(250, 10, 10);
   strokeWeight(5);
   pontinho.nextFrame();
+  strokeWeight(0.5);
+  pontinho.linhas();
 }
 
 class Ponto {
   constructor() {
     this.raio = -20;
     this.angulo = 0;
-    this.const = -0.111111;
+    this.const = 1;
   }
   nextFrame(){
-    push();
-    translate(oX, oY);
-    rotate(-this.angulo);
-    point(this.raio, 0);
-    this.angulo+=0.5;
-    console.log(this.angulo);
-    if(this.angulo > 180){
-      this.raio-=0.1111; 
+    if(this.angulo < 180){
+      push();
+      translate(oX, oY);
+      rotate(-this.angulo);
+      point(this.raio, 0);
+      this.angulo+=0.5;
+      pop();
     }
-    if(this.angulo > 360){
-      this.raio-=0.1111; 
+    if(this.angulo < 180){
+      push();
+      translate(oX - 20, oY);
+      rotate(-this.angulo);
+      point(-this.raio*2, 0);
+      this.angulo+=0.5;
+      pop();
     }
-    if(this.angulo > 540){
-      this.raio-=0.1111; 
+    if(this.angulo < 180){
+      push();
+      translate(oX + 20, oY);
+      rotate(-this.angulo);
+      point(this.raio*4, 0);
+      this.angulo-=0.5;
+      pop();
     }
-    pop();
+    if(this.angulo < 180){
+      push();
+      
+    }
   }
+    linhas(){
+      stroke(10, 10, 250);
+      line(0, HEIGHT/2, WIDTH, HEIGHT/2);
+      stroke(10, 250, 10);
+      line(WIDTH/2, 0, WIDTH/2, HEIGHT); 
+    }
 }
