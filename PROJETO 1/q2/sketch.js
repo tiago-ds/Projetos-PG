24ba5964bc495b7519ec2f4d3318eea37757837d
Tiago <tscs2@cin.ufpe.br>
2020-09-28 15:12:37 -0300

    Purifica código da q2

--- PROJETO 1/q2/sketch.js
+++ PROJETO 1/q2/sketch.js
@@ -2,11 +2,14 @@ let bracinho;
 let WIDTH = 500;
 let HEIGHT = 500;
 let startDate = new Date();
+let ok = false;
 
 function setup() {
   // Cria o canvas e o objeto do Braço
   createCanvas(WIDTH, HEIGHT);
-  bracinho = new Braco(20);
+  bracinho = new Braco(15);
+  angleMode(DEGREES);
+  frameRate(55);
 }
 
 function draw() {
@@ -15,92 +18,62 @@ function draw() {
 }
 
 class Braco{
+  
   constructor(UC){
     this.braco = new createVector(0, 2*UC); 
     this.antebraco = new createVector(0, 3*UC);
     this.angulo = 0;
-    this.const = 0.0045;
+    this.const = -0.5;
   }
+  
   update(){
-    /* Isso acontece por causa de JS.
-    /  A ideia era que o ângulo*90 deveria
-    /  ir até 45°. Mas como JS é TERRÍVEL
-    /  pra trabalhar com números quebrados 
-    /  - podemos perceber a discrepância 
-    /  gigantesca se descomentarmos a linha
-    /  40 -, tivemos que colocar 46 e 
-    /  trabalhar com o operador "<", mas
-    /  ainda assim não chegamos ao ângulo
-    /  exato.Multiplicamos a condição por 
-    /  90 que é o ângulo que engloba ir de 
-    /  totalmente vertical até totalmente 
-    /  horizontal.
-    */
-    if(this.angulo*90 < 46){
-      //console.log(this.angulo * 90);
-      if (frameCount == 0) {
-        startDate = new Date();
-      }
-      push();
-      // Transfere o centro de rotação para
-      // o meio do canvas.
-      translate(WIDTH/2, HEIGHT/2);
-      rotate(-this.angulo);
-      
-      // linha do braço
-      strokeWeight(2);
-      stroke(0);
-      line(0, 0, 0, this.braco.y);
-      strokeWeight(8);
-      stroke(255);
-      point(0, 0);
-      
-      // fim das operações para o braço
-      push();
-      translate(0, this.braco.y);
-      rotate(-this.angulo * 2);
-      
-      strokeWeight(2);
-      stroke(0);
-      line(0, 0, 0, this.antebraco.y);
-      
-      strokeWeight(8);
-      stroke(255);
-      point(0, 0);
-      point(0, this.antebraco.y);
-      
+    if(this.angulo > -45){
       this.angulo+=this.const;
-      pop();
-      pop();
+    }else{
+      if(!ok){
+        console.log(`TIME: ${new Date() - startDate}`);
+        ok = true;
+      }
+      
     }
-    else{
-      push()
-      translate(WIDTH/2, HEIGHT/2);
-      rotate(-this.angulo);
-      
-      strokeWeight(2);
-      stroke(0);
-      line(0, 0, 0, this.braco.y);
-      strokeWeight(8);
-      stroke(255);
-      point(0, 0);
-      
-      push();
-      translate(0, this.braco.y);
-      rotate(-this.angulo*2);
-      
-      strokeWeight(2);
-      stroke(0);
-      line(0, 0, 0, this.antebraco.y);
-      strokeWeight(8);
-      stroke(255);
-      point(0, 0);
-      point(0, this.antebraco.y);
-      
-      pop();
-      pop();
-      console.log(`DATE: ${new Date() - startDate}`);
-      //console.log(this.angulo * 90);
+    if (frameCount == 0) {
+      startDate = new Date();
     }
+    push();
+    // Transfere o centro de rotação para
+    // o meio do canvas.
+    translate(WIDTH/2, 2*HEIGHT/6);
+    
+    // Rotaciona 
+    rotate(this.angulo);
+    
+    // Linha do braço
+    strokeWeight(3);
+    stroke(0);
+    line(0, 0, 0, this.braco.y);
+    
+    // Ponto do braço
+    strokeWeight(10);
+    stroke(255);
+    point(0, 0);
+    
+    // Fim das operações pro braço
+    push();
+    translate(0, this.braco.y);
+    rotate(this.angulo);
+    
+    // Linha do antebraço
+    strokeWeight(3);
+    stroke(0);
+    line(0, 0, 0, this.antebraco.y);
+    
+    // Pontos do antebraço
+    strokeWeight(10);
+    stroke(255);
+    point(0, 0);
+    point(0, this.antebraco.y);
+    
+    pop();
+    pop();
   }
 }
