543fbd88781b656dcc5e2421db1040a253d55f2a
Tiago <tscs2@cin.ufpe.br>
2020-09-29 17:56:53 -0300

    Desenha eixo de coordenadas

--- PROJETO 1/q5/sketch.js
+++ PROJETO 1/q5/sketch.js
@@ -1,17 +1,29 @@
+// GUIA: 
+
+
 function setup() {
   createCanvas(800, 800, WEBGL);
   angleMode(DEGREES);
   background(220);
-  //rotateX(30);
+
+  strokeWeight(2);
+
+  // Ao redor da linha VERDE
+  rotateX(-30);
+  // Ao redor da linha AZUL
   rotateY(30);
+  
+  // Ao redor da linha VERMELHA
+  //rotateZ(30);
   stroke('green');
-  line(0, 0, 0, 400, 0, 0);
+  line(0, 0, 0, 200, 0, 0);
   stroke('blue');
-  line(0, -400, 0, 0, 0, 0);
+  line(0, -200, 0, 0, 0, 0);
   stroke('red');
-  line(0, 0, 400, 0, 0, 0);
+  line(0, 0, 200, 0, 0, 0);
+
 }
 
 function draw() {
-
+  //rect(0, 0, 200, 200);
 }
