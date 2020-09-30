476f9c0b7164007de35cde9d1c35b0b14037a01c
Tiago <tscs2@cin.ufpe.br>
2020-09-30 00:22:45 -0300

    Comenta código mas não organiza

--- PROJETO 1/q5/sketch.js
+++ PROJETO 1/q5/sketch.js
@@ -1,32 +1,45 @@
-// GUIA: 
-
 let angle = 0;
-let bRaio = 200;
+let bRaio = 100;
 function setup() {
   createCanvas(1000, 1000, WEBGL);
   angleMode(DEGREES);
   background(220);
-  frameRate(60);
+  perspective();
 }
 
 function draw() {
   background(255);
-  desenhasetup();
+
+  // Desenha linhas
+  desenhaSetup();
+
   push();
+  
   /* Alinha o eixo de desenho
   /  com os eixos já desenhados
   */ 
   rotateX(-30);
   rotateY(30);
 
- /* Rotaciona o eixo para alinhar
- /  o desenho do círculo e do 
- /  quadrado
- */ 
+  /* Rotaciona ao redor da linha
+  /  azul para alinhar com a linha
+  /  vermelha 
+  */ 
   rotateY(-90);
-  rotateX(240);
 
- // Desenha o círculo e o quadrado
+  /* Rotaciona 60° ao redor da linha
+  /  vermelha para desenhar o quadrado
+  /  como pedido na questão
+  /  (O valor aplicado é -150 pois já
+  /  haviam rotações aplicadas anterior-
+  /  mente)
+  */
+  rotateX(-150);
+  //rotateX(210);
+
+  /* Desenha o círculo, o quadrado e o
+  /  ponto no centro.
+  */
   noFill();
   strokeWeight(5);
   rect(0,0,200);
@@ -34,34 +47,43 @@ function draw() {
   circle(100, 100, 200);
   point(100, 100);
 
-  stroke('red');
+  // Move o eixo para o centro do círculo
   translate(100, 100);
 
-  // Deixa o eixo de rotação 2d perpendicular
-  // ao plano
-  //point(0, 0, 0);
+  /* Deixa o eixo de rotação 2d perpendicular
+  /  ao plano
+  */
   rotateZ(-angle);
-  //line(0,0,0, 0, -100, 0);
-
+  
   //circle(0, 0 , 0, 25);
+
+  // Move o eixo de até a linha do círculo maior
   translate(0, -100, -25);
-  strokeWeight(10);
-  stroke('red');
+
+  // Ponto no círculo menor
   rotateX(90);
+
+  // Rotaciona o círculo e o ponto no dobro da 
+  // velocidade 
   rotateZ(-angle*2);
-  point(0, -25);
-  //stroke('yellow');
+
+  // Desenha o círculo
   stroke(0);
   strokeWeight(5);
   noFill();
   circle(0, 0, 50);
 
+  // Desenha o ponto no círculo
+  strokeWeight(10);
+  stroke('red');
+  point(0, -25);
 
   pop();
-  angle -= 2.5;
+  angle -= 2;
 }
 
-function desenhasetup(){
+function desenhaSetup(){
+  // Linha mais fina
   strokeWeight(2);
   
   push();
@@ -75,7 +97,7 @@ function desenhasetup(){
   // Ao redor da linha VERMELHA
   //rotateZ(30);
 
-  // Desenha as três linhas
+  // Desenha as três linhas do eixo
   stroke('green');
   line(0, 0, 0, 400, 0, 0);
   stroke('blue');
