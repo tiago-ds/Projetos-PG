4d2432ce9caa581430b92d728d395f4c479a2fd8
Tiago <tscs2@cin.ufpe.br>
2020-10-31 18:46:23 -0300

    Eu nem sei mais/2

--- PROJETO 2/sketch.js
+++ PROJETO 2/sketch.js
@@ -1,33 +1,74 @@
+// TODO:
+// Lista que salva as curvas pra poder selecionar qualquer uma delas
+// Campo pra trocar o valor de numero_avaliacoes
+// Botão de alterar curva escolhida
+
 const WIDTH = 1000;
 const HEIGHT = 600;
 
+let BGC1;
+let BGC2;
+
+let construindo = false;
+let selecionada;
+
+// Array com as curvas
 let curvas = [];
+
+// WIP array dos pontos de controle sendo adicionados agr
 let pontos_controle = [];
 
+// WIP variável de seleção do número de Avaliações
 let numero_avaliacoes = 300;
+
+//Flags para mostrar ou não essas coisas aí
+let FlagPontosControle = true;
+let FlagPoligonaisControle = false;
+let FlagCurvas = true;
+
+let btn_CriarCurva = document.getElementById('newCurve');
+let btn_SelecionarCurva = document.getElementById('selectCurve');
+let btn_DeletarCurva = document.getElementById('deleteCurve');
+
+let box_PontosControle = document.getElementById('controlPoints');
+let box_PoligonaisControle = document.getElementById('polygonalPoints');
+let box_Curvas = document.getElementById('curves');
+
+btn_CriarCurva.onclick = criar_curva;
+btn_SelecionarCurva.onclick = selecionar_curva; 
+btn_DeletarCurva.onclick = deletar_curva;
+
+box_PontosControle.onchange = TogglePontosControle;
+box_PoligonaisControle.onchange = TogglePoligonaisControle;
+box_Curvas.onchange = ToggleCurvas;
 
 let butao;
 
 function setup() {
   createCanvas(WIDTH, HEIGHT);
-  background(188);
-  butao = createButton('generate').position(50, 50);
-  butao.mousePressed(Generate);
+  BGC1 = color(183, 206, 232);
+  BGC2 = color(247, 247, 247);
 }
 
 function draw() {
+  setGradient(0, 0, WIDTH, HEIGHT, BGC1, BGC2, 1);
+  stroke(0);
   for(ponto in pontos_controle){
     pontos_controle[ponto].display();
   }
   for(curva in curvas){
-    curvas[curva].display_curva();
-    //curvas[curva].display_pontos();
-    //curvas[curva].display_poligonais();
+    if(FlagCurvas)
+      curvas[curva].display_curva();
+    if(FlagPontosControle)
+      curvas[curva].display_pontos();
+    if(FlagPoligonaisControle)
+      curvas[curva].display_poligonais();
   }
 }
 
 function mouseClicked() {
-  pontos_controle.push(new Ponto(mouseX, mouseY));
+  if(mouseX >= 0 && mouseX <= WIDTH && construindo)
+    pontos_controle.push(new Ponto(mouseX, mouseY));
 }
 
 function castelinho(pontos, x) {
@@ -46,29 +87,20 @@ function castelinho(pontos, x) {
   }
 }
 
-
-function Generate(){
-  if(pontos_controle.length <= 1)
-    return;
-  let c = new Curva();
-  for(x = 0; x <= numero_avaliacoes; x++){
-    let ponto = castelinho(pontos_controle, x);
-    c.pontos_avaliacao.push({ x: ponto.x, y: ponto.y });
-    c.pontos_controle = pontos_controle;
-  }
-  curvas.push(c);
-  pontos_controle = [];
-}
-
 class Curva{
   constructor(){
     //O tamanho de pontos_Avaliacao será de n+1 avaliações,
     //pois ele serve de base para as linhas de avaliação.
     this.pontos_avaliacao = [];
     this.pontos_controle = [];
+
+    this.cor = 0;
+
+    this.secionada = false;
   }
   display_curva(){
     strokeWeight(2);
+    stroke(this.cor);
     for(x = 0; x < this.pontos_avaliacao.length - 1; x++){
       line(this.pontos_avaliacao[x].x,
            this.pontos_avaliacao[x].y,
@@ -78,12 +110,14 @@ class Curva{
   }
   display_pontos(){
     strokeWeight(5);
+    stroke(0);
     for(x = 0; x < this.pontos_controle.length; x++){
       point(this.pontos_controle[x].x, this.pontos_controle[x].y);
     }
   }
   display_poligonais(){
     strokeWeight(1);
+    stroke(0);
     for(x = 0; x < this.pontos_controle.length - 1; x++){
       line(this.pontos_controle[x].x, 
            this.pontos_controle[x].y,
@@ -102,4 +136,85 @@ class Ponto{
     strokeWeight(5);      
     point(this.x, this.y);
   }
+}
+
+function TogglePontosControle() {
+  if(FlagPontosControle)
+    FlagPontosControle = false;
+  else
+    FlagPontosControle = true;
+}
+
+function TogglePoligonaisControle(){
+  if(FlagPoligonaisControle)
+    FlagPoligonaisControle = false;
+  else
+    FlagPoligonaisControle = true;
+}
+
+function ToggleCurvas(){
+  if(FlagCurvas)
+    FlagCurvas = false;
+  else
+    FlagCurvas = true;
+}
+
+function criar_curva(){
+  if(construindo){
+    if(pontos_controle.length > 1){
+      Generate(pontos_controle);
+      construindo = false;
+      btn_CriarCurva.style.backgroundColor = 'white';
+    }
+    return;
+  }
+  construindo = true;
+  btn_CriarCurva.style.backgroundColor = 'red';
+}
+
+function selecionar_curva(){
+  let index = prompt('Qual curva?');
+  if(index >= 0 && curvas.length > index){
+    curvas[index].selected = true;
+    selecionada = index;
+  }
+  else
+    alert('Essa curva não existe');
+}
+
+function deletar_curva(){
+    if(selecionada){
+        curvas.splice(selecionada, 1);
+        selecionada = undefined;
+    }else{
+        alert('SELECIONA A PORRA DA CURVA');
+    }
+}
+
+function Generate(){
+  let c = new Curva();
+  for(x = 0; x <= numero_avaliacoes; x++){
+    let ponto = castelinho(pontos_controle, x);
+    c.pontos_avaliacao.push(new Ponto(ponto.x, ponto.y));
+    c.pontos_controle = pontos_controle;
+  }
+  curvas.push(c);
+  pontos_controle = [];
+}
+
+function setGradient(x, y, w, h, c1, c2) {
+  noFill();
+  for (let i = y; i <= y + h; i++) {
+    let inter = map(i, y, y + h, 0, 1);
+    let c = lerpColor(c1, c2, inter);
+    stroke(c);
+    line(x, i, x + w, i);
+  }
+}
+
+function check_near(P, mX, mY){
+    console.log(mouseX, P.x)
+    if((mX <= (P.x + 4) && mX >= (P.x - 4)) && (mY <= (P.y + 4) && mY >= (P.y - 4)))
+        return true;
+    return false;
 }
