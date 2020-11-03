78d3aeb9dea68a50a6218a12f3cc19f54a4eb9c9
Tiago <tscs2@cin.ufpe.br>
2020-11-03 14:05:57 -0300

    Agora as coisas funcionam bonitinho

--- PROJETO 2/sketch.js
+++ PROJETO 2/sketch.js
@@ -17,27 +17,29 @@
 const WIDTH = 1000;
 const HEIGHT = 600;
 
+// Cores do gradiente que serão inicializadas em setup();
 let BGC1;
 let BGC2;
 
-let construindo = false;
-
-let selecionada;
-let selecionado;
+// A gente não sabe usar recursão, então vamos de variável global
+let curva_selecionada;
+let ponto_selecionado;
 
 // Array com as curvas
 let curvas = [];
 
-// WIP array dos pontos de controle sendo adicionados agr
-let pontos_controle = [];
+// Array dos pontos de controle sendo adicionados agr
+let pontos_controle;
 
 // WIP variável de seleção do número de Avaliações
-let numero_avaliacoes = 100;
+let numero_avaliacoes = 200;
 
 //Flags para mostrar ou não essas coisas aí
-let FlagPontosControle = true;
-let FlagPoligonaisControle = false;
-let FlagCurvas = true;
+let flag_pontos_controle = true;
+let flag_poligonais_controle = false;
+let flag_curvas = true;
+
+let selecionando_curva = false;
 
 let btn_CriarCurva = document.getElementById('newCurve');
 let btn_SelecionarCurva = document.getElementById('selectCurve');
@@ -68,50 +70,57 @@ function draw() {
     pontos_controle[ponto].display();
   }
   for(curva in curvas){
-    if(FlagCurvas)
+    if(flag_curvas)
       curvas[curva].display_curva();
-    if(FlagPontosControle)
-      curvas[curva].display_pontos();
-    if(FlagPoligonaisControle)
+    if(flag_poligonais_controle)
       curvas[curva].display_poligonais();
+    if(flag_pontos_controle)
+      curvas[curva].display_pontos();
   }
 }
 
+// Funções do mouse //
 function mouseClicked() {
-  if(mouseX >= 0 && mouseX <= WIDTH && construindo)
+  if(selecionando_curva){
+    procurar_curva();
+  }
+  if(mouseX >= 0 && mouseX <= WIDTH && pontos_controle != undefined)
     pontos_controle.push(new Ponto(mouseX, mouseY));
 }
 
 function mousePressed() {
-  if(selecionada){
-    for(const p of curvas[selecionada].pontos_controle){
+  if(curva_selecionada != undefined){
+    for(const p of curvas[curva_selecionada].pontos_controle){
       if(check_near(p, mouseX, mouseY)){
         p.locked = true;
-        selecionado = curvas[selecionada].pontos_controle.indexOf(p);
-        console.log(selecionado)
+        ponto_selecionado = curvas[curva_selecionada].pontos_controle.indexOf(p);
+        return;
       }
     }
   }
 }
 
-function  mouseDragged() {
-  if(selecionado != undefined){
-    for(const p of curvas[selecionada].pontos_controle){
+function mouseDragged() {
+  if(ponto_selecionado != undefined){
+    for(const p of curvas[curva_selecionada].pontos_controle){
         if(p.locked){
           p.x = mouseX;
           p.y = mouseY;
+          curvas[curva_selecionada].regenerate();
       }
     }
   }
 }
 
 function mouseReleased() {
-  if(selecionado != undefined){
-    curvas[selecionada].pontos_controle[selecionado].locked = false;
-    selecionado = undefined;
+  if(ponto_selecionado != undefined){
+    curvas[curva_selecionada].pontos_controle[ponto_selecionado].locked = false;
+    ponto_selecionado = undefined;
   }
 }
 
+// De casteljau. Ela recebe um array de pontos, e retorna um ponto.
+// O X seria o equivalente ao parâmetro
 function castelinho(pontos, x) {
   if (pontos.length > 1) {
     let aux = [];
@@ -140,6 +149,10 @@ class Curva{
     this.secionada = false;
   }
 
+  regenerate(){
+    this.pontos_avaliacao = Generate(this.pontos_controle).pontos_avaliacao;
+  }
+
   display_curva(){
     strokeWeight(2);
     stroke(this.cor);
@@ -156,6 +169,8 @@ class Curva{
   display_pontos(){
     strokeWeight(5);
     stroke(0);
+    if(this.selecionada)
+      stroke(250, 50, 50);
     for(x = 0; x < this.pontos_controle.length; x++){
       point(this.pontos_controle[x].x, this.pontos_controle[x].y);
     }
@@ -184,67 +199,77 @@ class Ponto{
   }
 }
 
+//Checkboxes
 function TogglePontosControle() {
-  if(FlagPontosControle)
-    FlagPontosControle = false;
+  if(flag_pontos_controle)
+    flag_pontos_controle = false;
   else
-    FlagPontosControle = true;
+    flag_pontos_controle = true;
 }
 
 function TogglePoligonaisControle(){
-  if(FlagPoligonaisControle)
-    FlagPoligonaisControle = false;
+  if(flag_poligonais_controle)
+    flag_poligonais_controle = false;
   else
-    FlagPoligonaisControle = true;
+    flag_poligonais_controle = true;
 }
 
 function ToggleCurvas(){
-  if(FlagCurvas)
-    FlagCurvas = false;
+  if(flag_curvas)
+    flag_curvas = false;
   else
-    FlagCurvas = true;
+    flag_curvas = true;
 }
 
+//Botões de criar, selecionar e deletar curva.
 function criar_curva(){
-  if(selecionada != undefined){
-    curvas[selecionada].selecionada = false;
-    selecionada = undefined;
+  // Condição que não deixa que tenha uma curva selecionada na hora de 
+  // criar uma curva nova.
+  if(curva_selecionada != undefined){
+    curvas[curva_selecionada].selecionada = false;
+    curva_selecionada = undefined;
   }
-  if(construindo){
+  // Caso já esteja sendo criada uma curva
+  if(pontos_controle != undefined){
+    // Uma curva é composta por pelo menos 2 pontos de controle.
     if(pontos_controle.length > 1){
       curvas.push(Generate(pontos_controle));
-      construindo = false;
+      pontos_controle = undefined;
       btn_CriarCurva.style.backgroundColor = 'white';
-      pontos_controle = [];
+    }else{
+      return;
     }
-    return;
+  }else{
+    pontos_controle = [];
+    btn_CriarCurva.style.backgroundColor = 'red';
   }
-  construindo = true;
-  btn_CriarCurva.style.backgroundColor = 'red';
 }
 
 function selecionar_curva(){
-  if(selecionada)
-    curvas[selecionada].selecionada = false;
-  let index = prompt('Qual curva?');
-  if(index >= 0 && curvas.length > index){
-    curvas[index].selecionada = true;
-    selecionada = index;
+  // Condição que faz com que não seja permitido selecionar uma curva
+  // enquanto constrói uma
+  if(pontos_controle)
+    return;
+  if(curva_selecionada != undefined){
+    curvas[curva_selecionada].selecionada = false;
+    curva_selecionada = undefined;
   }
-  else
-    alert('Essa curva não existe');
+  if(!selecionando_curva){
+    selecionando_curva = true;
+    btn_SelecionarCurva.style.backgroundColor = 'red';
+  }
 }
 
 function deletar_curva(){
-    if(selecionada){
-        curvas.splice(selecionada, 1);
-        selecionada = undefined;
+    if(curva_selecionada != undefined){
+        curvas.splice(curva_selecionada, 1);
+        curva_selecionada = undefined;
     }else{
         alert('SELECIONA A PORRA DA CURVA');
     }
 }
 
-//vai me dar uma curva que tem os pontos de avaliação e os pontos de controle
+// Vai criar uma curva através de vários Decasteljau
 function Generate(pontos_controle){
   let c = new Curva();
   for(x = 0; x <= numero_avaliacoes; x++){
@@ -255,7 +280,7 @@ function Generate(pontos_controle){
   return c;
 }
 
-// pra desenhar um gradiente de fundo bonitinho kk
+// Pra desenhar um gradiente de fundo bonitinho kk
 
 function setGradient(x, y, w, h, c1, c2) {
   noFill();
@@ -267,8 +292,43 @@ function setGradient(x, y, w, h, c1, c2) {
   }
 }
 
+
+// Funções de utilidade pra deixar o código principal mais limpo
 function check_near(P, mX, mY){
-    if((mX <= (P.x + 4) && mX >= (P.x - 4)) && (mY <= (P.y + 4) && mY >= (P.y - 4)))
-        return true;
-    return false;
+  // Vê se tem um ponto perto do mouse
+  if((mX <= (P.x + 4) && mX >= (P.x - 4)) && (mY <= (P.y + 4) && mY >= (P.y - 4)))
+    return true;
+  return false;
+}
+
+function procurar_curva(){
+  // Ele olha dentro de cada ponto de avaliação de cada curva, até achar alguma próxima
+  // do mouse.
+  for(const c of curvas){
+    for(const p of c.pontos_avaliacao){
+      if(check_near(p, mouseX, mouseY)){
+
+        curva_selecionada = curvas.indexOf(c);
+        c.selecionada = true;
+
+        selecionando_curva = false;
+
+        btn_SelecionarCurva.style.backgroundColor = 'white';
+
+        return;
+      }
+    }
+    for(const p of c.pontos_controle){
+      if(check_near(p, mouseX, mouseY)){
+        curva_selecionada = curvas.indexOf(c);
+        c.selecionada = true;
+
+        selecionando_curva = false;
+
+        btn_SelecionarCurva.style.backgroundColor = 'white';
+
+        return;
+      }
+    }
+  }
 }
